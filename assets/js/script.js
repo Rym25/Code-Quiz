c439550a810e88635f425208cb969b527b8935cd
Ryely <ryely.moore@outlook.com>
2021-05-21 19:52:13 -0600

    page starts the quiz and cycles through questions, also added a timer that starts when start quiz is pressed and stops at zero

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,34 @@
 // Gets the html elements we will work with from the page
 var mainEl = document.querySelector("#questions");
 var articleEl = document.querySelector("#options");
-var timeEl = document.querySelector("#timer");
+var timeEl = document.querySelector(".timer");
 
+// creates an i for iterating
+var i = 0;
+
+// creates an array with the questions, options, and answers
+var qArr = [
+    {question: "Commonly used Data Types do not include:", a: "strings", b: "booleans", c: "alerts", d: "numbers", answer: "alerts"},
+    {question: "A very useful tool during development and debugging for printing content to the debugger is:", a: "JavaScript", b: "terminal/bash", c: "for loops", d: "console.log", answer: "console.log"},
+    {question: "Arrays in JavaScript can be used to store:", a: "numbers and strings", b: "booleans", c: "other arrays", d: "all of the above", answer: "all of the above"},
+    {question: "String values must be enclosed by _______ when being assigned to a variable:", a: "quotes", b: "commas", c: "curly brackets", d: "parenthesis", answer: "quotes"},
+    {question: "The condition in an if/else statement must be enclosed by _______:", a: "quotes", b: "parenthesis", c: "square brackets", d: "curly brackets", answer: "parenthesis"}    
+            ];
+
+// function to style all of the buttons
 var styleButton = function (button) {
-    
+    button.style.borderRadius = "6px";
+    button.style.backgroundColor = "blueviolet";
+    button.style.color = "antiquewhite"
 }
+
+// Function to remove all of the childern of an element
+var removeElements = function(parent) {
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+};
+
 // Function that sets the starting state
 var startState = function () {
     
@@ -25,14 +48,62 @@ var startState = function () {
     var startButtonEl = document.createElement("button");
     startButtonEl.textContent = "Start Quiz";
     startButtonEl.setAttribute("id", "start-button");
-    startButtonEl.style.borderRadius = "6px";
-    startButtonEl.style.backgroundColor = "blueviolet";
-    startButtonEl.style.color = "antiquewhite"
+    styleButton(startButtonEl);
     articleEl.appendChild(startButtonEl);
 }
 
+// create a function for the quiz state
+var nextQuestion = function (i) {
+    // Clears childern from the main and article elements
+    removeElements(mainEl);
+    removeElements(articleEl);
+
+    // gets question and puts it in the main in an h1
+    var h1El = document.createElement("h1");
+    h1El.textContent = qArr[i].question;
+    h1El.style.textAlign = "Center"
+    mainEl.appendChild(h1El);
+
+    // creates the options
+    for (var q = 0; q < 4; q++) {
+        var arr = ["a","b","c","d"];
+        var ButtonEl = document.createElement("button");
+        ButtonEl.textContent = qArr[i][arr[q]];
+        ButtonEl.setAttribute("class", "button");
+        styleButton(ButtonEl);
+        articleEl.appendChild(ButtonEl);
+    }
+    i++;
+    return i;
+}
+
+var buttonHandler = function (event) {
+    // gets the target of the event
+    var targetEl = event.target;
+    // does an action based on the event
+    if (targetEl.matches("#start-button")){
+        i = nextQuestion(i);
+        var timeVar = setInterval(function () {
+            var time = document.getElementById("timer").textContent;
+            var timeMan = parseInt(time);
+            timeMan--;
+            timeMan.toString();
+            if (timeMan <= 0) {
+                clearInterval(timeVar);
+                timeMan = "0";
+            }
+            timeEl.innerHTML = "<p>Time:<span id='timer'>" + timeMan + "</span></p>"
+        }, 1000);
+    } else if (targetEl.matches(".button") && i !== 5) {
+        i = nextQuestion(i);
+    } else {
+        console.log("done with the quiz");
+    }
+
+
+}
+// Calls startState when the page is loaded
 startState();
 
-document.querySelector("#start-button").addEventListener("click", function() {
-    console.log("start the quiz");
-})
+// Event Listener for the start button
+articleEl.addEventListener("click", buttonHandler);
